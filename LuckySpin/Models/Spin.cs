71c870e05bdd0f7cc19bbd5060d056ded85387e0
Brian Bansenauer <brianb@cascadia.edu>
2026-01-12 13:28:25 -0800

    updates to model default values and view logic

--- LuckySpin/Models/Spin.cs
+++ LuckySpin/Models/Spin.cs
@@ -4,7 +4,7 @@ namespace LuckySpin.Models;
 public class Spin
 {
     // Properties (notice their data types)
-    public int Luck { get; set; }
-    public int[]? Numbers { get; set; } //The question mark means that these properties are nullable
-    public string? ImageDisplay { get; set; }
+    public int Luck { get; set; } = 7; //Default to 7
+    public required int[] Numbers { get; set; } //Made required to ensure it's always set or an error is thrown
+    public string ImageDisplay { get; set; } = "none"; //Default to "none" so the image is hidden unless set to "block"
 }
