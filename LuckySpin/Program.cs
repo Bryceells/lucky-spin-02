78850033b7b3f5a308a631ecf3f5c6cdb9804d87
Bryceells <bryceells@live.com>
2026-01-13 15:27:03 -0800

    Added ability to spin numbers and updated display for them.

--- LuckySpin/Program.cs
+++ LuckySpin/Program.cs
@@ -2,11 +2,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 /* Install Services using the builder.Services methods */
-//TODO: use the AddMvc Service to enable MVC for this application
+// use the AddMvc Service to enable MVC for this application
+builder.Services.AddMvc();
 
 
-//TODO: DIJ Part 1: Register the TextTranform class as available for DIJ using th AddTransient Service
-
+//DIJ Part 1: Register the TextTranform class as available for DIJ using th AddTransient Service;
+builder.Services.AddTransient<TextTransform>();
 
 var app = builder.Build();
 
